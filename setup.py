b62ffa6c1976ac15747520bf9fe230c70d7bb4f8
Manuel Buil Mur <manuel.buil@ericsson.com>
2015-10-09 16:48:36 +0200

    First version of VNF manager simulator
    This simulator contacts Openstack to create the SF VMs and then contacts ODL to create the SFC.
    It is not intended to be a replacement of other VNF Managers such as Tacker or OpenMANO, just a
    simple simulation for testing and development purposes.
    
    Change-Id: Iaf691035d1b8435f5fccf559aebf6f3ed1afd066
    Signed-off-by: mbuil <manuel.buil@ericsson.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "vnf manager simulator",
+    version = "1.0",
+    author = "Manuel Buil, Brady A. Johnson",
+    author_email = "manuel.buil@ericsson.com, brady.allen.johnson@ericsson.com",
+    description = ("A script which simulates an orchestrator"),
+    license = "Apache License Version 2.0",
+    keywords = "example documentation tutorial",
+    url = "https://gerrit.opnfv.org/gerrit/#/c/2519",
+    packages = [".."],
+    scripts = [".."],
+    long_description=read('README'),
+)
