73c4d745bf2bba060dc8301a9127482f01318de1
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-15 00:18:32 +0100

    Fix all the flake8 violations in the repo
    Change-Id: I05109298835c26a1927a490f8c21dd2ca7f77947
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- tests/NAME_tests.py
+++ tests/NAME_tests.py
@@ -1,11 +1,11 @@
-from nose.tools import *
-import NAME
 
 def setup():
-	print "SETUP!"
+    print "SETUP!"
+
 
 def teardown():
-	print "TEAR DOWN!"
+    print "TEAR DOWN!"
+
 
 def test_basic():
-	print "I RAN!"
+    print "I RAN!"
