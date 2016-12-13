424379a56d6b9734d4fe316c4660765447841283
George Paraskevopoulos <geopar@intracom-telecom.com>
2016-12-13 15:50:25 +0200

    Use timethis from functest utils
    timethis is hosted now in the functest code. Use that and delete the
    local function
    
    Change-Id: Icfdfa0609cd5ac93e6852e77e09bc4b901894f18
    Signed-off-by: George Paraskevopoulos <geopar@intracom-telecom.com>

--- tests/functest/odl-sfc/utils.py
+++ tests/functest/odl-sfc/utils.py
@@ -7,7 +7,6 @@ import functest.utils.openstack_utils as os_utils
 import re
 import json
 import SSHUtils as ssh_utils
-import functools
 
 
 logger = ft_logger.Logger("sfc_test_utils").getLogger()
@@ -341,21 +340,7 @@ def check_ssh(ips, retries=100):
     return False
 
 
-def timethis(func):
-    """Measure the time it takes to update the classification rules"""
-    @functools.wraps(func)
-    def timed(*args, **kwargs):
-        ts = time.time()
-        result = func(*args, **kwargs)
-        te = time.time()
-        elapsed = '{0}'.format(te - ts)
-        logger.info('{f}(*{a}, **{kw}) took: {t} sec'.format(
-            f=func.__name__, a=args, kw=kwargs, t=elapsed))
-        return result
-    return timed
-
-
-@timethis
+@ft_utils.timethis
 def capture_time_log(ovs_logger, compute_clients, timeout=200):
     rsps = ovs_logger.ofctl_time_counter(compute_clients[0])
     first_RSP = rsps[0] if len(rsps) > 0 else ''
