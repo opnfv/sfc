4613b5572c380d11df7dea58839433eb2c39f36f
“Manuel Buil” <mbuil@suse.com>
2017-03-16 11:52:56 +0100

    Bug fix: disable non-working topologies
    JIRA: SFC-87
    
    Due to ODL bugs, some topologies do not work
    
    Change-Id: Ia2fcfe5948c308ce2348b52c01aa3ce58ac318d6
    Signed-off-by: “Manuel Buil” <mbuil@suse.com>
    (cherry picked from commit 184f787f3a8a57695fa3b9b8f17c65b9aeee1904)

--- sfc/lib/topology_shuffler.py
+++ sfc/lib/topology_shuffler.py
@@ -16,9 +16,16 @@ TOPOLOGIES = [
         '''
     },
     {
+        'id': 'CLIENT_SERVER_SAME_HOST_SPLIT_VNF',
+        'description': '''
+        Client and server are on the same host.
+        The VNFs are split between hosts Round Robin.
+        '''
+    },
+    {
         'id': 'CLIENT_VNF_SAME_HOST',
         'description': '''
-        Client instance and vnfs are are on the same
+        Client instance and vnfs are on the same
         compute host. Server instance is on a different host
         '''
     },
@@ -32,15 +39,8 @@ TOPOLOGIES = [
     {
         'id': 'SERVER_VNF_SAME_HOST',
         'description': '''
-        Server instance and vnfs are are on the same
+        Server instance and vnfs are on the same
         compute host. Client instance is on a different host
-        '''
-    },
-    {
-        'id': 'CLIENT_SERVER_SAME_HOST_SPLIT_VNF',
-        'description': '''
-        Client and server are on the same host.
-        The VNFs are split between hosts Round Robin.
         '''
     },
     {
@@ -67,7 +67,10 @@ def get_seed():
     NOTE: There's sure a smarter way to do this
           Probably with the Jenkins job id
     '''
-    cutoff = len(TOPOLOGIES) - 1
+    # cutoff = len(TOPOLOGIES) - 1
+    # We only add the topologies which are working
+    topologies_working = 2
+    cutoff = topologies_working - 1
     seed = datetime.datetime.today().weekday()
     if seed > cutoff:
         seed = random.randrange(cutoff)
