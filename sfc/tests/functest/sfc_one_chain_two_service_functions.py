0ca6bc3e838ca8e5e5c709d6bf9496be8fac39b6
Dimitrios Markou <mardim@intracom-telecom.com>
2017-09-01 14:26:45 +0300

    [WIP] Adapt functionality to the new Tacker API
    Change-Id: I078e9f26d3040cf04120abd425f03cb4f2e8daef
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

--- sfc/tests/functest/sfc_one_chain_two_service_functions.py
+++ sfc/tests/functest/sfc_one_chain_two_service_functions.py
@@ -107,7 +107,8 @@ def main():
 
     vnfs = ['testVNF1', 'testVNF2']
 
-    topo_seed = topo_shuffler.get_seed()
+    #topo_seed = topo_shuffler.get_seed()
+    topo_seed = 1 
     testTopology = topo_shuffler.topology(vnfs, seed=topo_seed)
 
     logger.info('This test is run with the topology {0}'
@@ -127,6 +128,9 @@ def main():
     logger.info("Client instance received private ip [{}]".format(client_ip))
     server_ip = server_instance.networks.get(TESTCASE_CONFIG.net_name)[0]
     logger.info("Server instance received private ip [{}]".format(server_ip))
+    
+    test_utils.create_tacker_vim_file(vim_file=COMMON_CONFIG.vim_file)
+    os_tacker.create_vim(tacker_client,vim_file=COMMON_CONFIG.vim_file)
 
     tosca_file = os.path.join(COMMON_CONFIG.sfc_test_dir,
                               COMMON_CONFIG.vnfd_dir,
@@ -134,14 +138,14 @@ def main():
 
     os_tacker.create_vnfd(
         tacker_client,
-        tosca_file=tosca_file)
+        tosca_file=tosca_file, vnfd_name='test-vnfd1')
 
     tosca_file = os.path.join(COMMON_CONFIG.sfc_test_dir,
                               COMMON_CONFIG.vnfd_dir,
                               TESTCASE_CONFIG.test_vnfd_blue)
     os_tacker.create_vnfd(
         tacker_client,
-        tosca_file=tosca_file)
+        tosca_file=tosca_file, vnfd_name='test-vnfd2')
 
     default_param_file = os.path.join(
         COMMON_CONFIG.sfc_test_dir,
@@ -156,30 +160,44 @@ def main():
         default_param_file, testTopology[vnfs[1]])
 
     vnf1_id = os_tacker.wait_for_vnf(tacker_client, vnf_name=vnfs[0])
+    print vnf1_id
     vnf2_id = os_tacker.wait_for_vnf(tacker_client, vnf_name=vnfs[1])
+    print vnf2_id
     if vnf1_id is None or vnf2_id is None:
         logger.error('ERROR while booting vnfs')
         sys.exit(1)
 
-    vnf1_instance_id = test_utils.get_nova_id(tacker_client, 'vdu1', vnf1_id)
+    vnf1_instance_id = test_utils.get_nova_id(tacker_client, 'VDU1', vnf1_id)
+    print vnf1_instance_id
     os_utils.add_secgroup_to_instance(nova_client, vnf1_instance_id, sg_id)
 
-    vnf2_instance_id = test_utils.get_nova_id(tacker_client, 'vdu1', vnf2_id)
-    os_utils.add_secgroup_to_instance(nova_client, vnf2_instance_id, sg_id)
+    vnf2_instance_id = test_utils.get_nova_id(tacker_client, 'VDU1', vnf2_id)
+    print vnf2_instance_id
+    pame = os_utils.add_secgroup_to_instance(nova_client, vnf2_instance_id, sg_id)
+    print 'einai: '+ str(pame)
+    
 
-    os_tacker.create_sfc(tacker_client, 'red',
-                         chain_vnf_names=[vnfs[0], vnfs[1]])
+    #os_tacker.create_sfc(tacker_client, 'red',
+    #                     chain_vnf_names=[vnfs[0], vnfs[1]])
 
-    os_tacker.create_sfc_classifier(
-        tacker_client, 'red_http', sfc_name='red',
-        match={
-            'source_port': 0,
-            'dest_port': 80,
-            'protocol': 6
-        })
+    tosca_file = os.path.join(COMMON_CONFIG.sfc_test_dir,
+                              COMMON_CONFIG.vnffgd_dir,
+                              TESTCASE_CONFIG.test_vnffgd_red)
 
-    logger.info(test_utils.run_cmd('tacker sfc-list')[1])
-    logger.info(test_utils.run_cmd('tacker sfc-classifier-list')[1])
+    os_tacker.create_vnffgd(tacker_client,tosca_file=tosca_file,vnffgd_name='red')
+
+    os_tacker.create_vnffg(tacker_client, vnffgd_name= 'red', vnffg_name='red_http')
+
+    #os_tacker.create_sfc_classifier(
+    #    tacker_client, 'red_http', sfc_name='red',
+    #    match={
+    #        'source_port': 0,
+    #        'dest_port': 80,
+    #        'protocol': 6
+    #    })
+
+    #logger.info(test_utils.run_cmd('tacker sfc-list')[1])
+    #logger.info(test_utils.run_cmd('tacker sfc-classifier-list')[1])
 
     # Start measuring the time it takes to implement the classification rules
     t1 = threading.Thread(target=test_utils.wait_for_classification_rules,
@@ -275,5 +293,5 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.config.fileConfig(COMMON_CONFIG.functest_logging_api)
+    #logging.config.fileConfig(COMMON_CONFIG.functest_logging_api)
     main()
