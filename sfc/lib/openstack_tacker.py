176f249a423524617e3f6e6134bf01460a4c6625
Manuel Buil <mbuil@suse.com>
2017-09-14 18:56:18 +0200

    Create vnffg providing the neutron_port
    JIRA: SFC-107
    
    We are submitting the vnffgd with a variable for the neutron_port. Before
    creating the vnffg, we must provide a file with the neutron_port value
    
    Change-Id: I9994e2baa53f323df17434448d372598c0d94c72
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- sfc/lib/openstack_tacker.py
+++ sfc/lib/openstack_tacker.py
@@ -249,10 +249,9 @@ def create_vnffgd(tacker_client, tosca_file=None, vnffgd_name=None):
 
 
 def create_vnffg(tacker_client, vnffg_name=None, vnffgd_id=None,
-                 vnffgd_name=None):
+                 vnffgd_name=None, param_file=None):
     '''
-      Tacker doesn't support Symmetrical chain and parameter file
-      in Openstack/Ocata
+      Creates the vnffg which will provide the RSP and the classifier
     '''
     try:
         vnffg_body = {
@@ -261,6 +260,11 @@ def create_vnffg(tacker_client, vnffg_name=None, vnffgd_id=None,
                 'name': vnffg_name
             }
         }
+        if param_file is not None:
+            params = None
+            with open(param_file) as f:
+                params = f.read()
+            vnffg_body['vnffg']['attributes']['param_values'] = params
         if vnffgd_id is not None:
             vnffg_body['vnffg']['vnffgd_id'] = vnffgd_id
         else:
