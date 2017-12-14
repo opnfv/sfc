794613f88970503229725559e1a8feb82e4f4f83
Manuel Buil <mbuil@suse.com>
2017-12-14 12:13:19 +0100

    [WIP] Migrate to SNAPs openstack library
    Functest is deprecating its openstack library which we were using extensively
    We need then to move to an alternative and the best appears to be SNAPs
    
    Change-Id: Icaa0b9f1ec580545b9d0faa88be2080a310deaf7
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- sfc/lib/config.py
+++ sfc/lib/config.py
@@ -87,7 +87,9 @@ class CommonConfig(object):
             "defaults.image_file_name", self.config_file)
         self.image_format = ft_utils.get_parameter_from_yaml(
             "defaults.image_format", self.config_file)
-        self.url = ft_utils.get_parameter_from_yaml(
+        self.image_url = ft_utils.get_parameter_from_yaml(
+            "defaults.image_url", self.config_file)
+         self.url = ft_utils.get_parameter_from_yaml(
             "defaults.url", self.config_file)
         self.dir_functest_data = ft_utils.get_functest_config(
             "general.dir.functest_data")
