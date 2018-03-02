4317063ca5750f0611ada0a7bc5a5e6a12fec845
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-03-02 18:14:37 +0100

    Support sfp redirect action on acl flow check
    Starting Oxygen, ODL networking-sfc translator supports symmetric
    paths and maps flow classifiers to ACLs with SFP redirect action
    instead of RSP redirect action. A SFP may have two RSPs associated
    if the path is symmetric, one otherwise. One of the two RSPs will
    be flagged as the reverse path and the other is the forward path.
    Traffic from the logical-source-port will be classified to the forward
    path, while traffic from the logical-destination-port will be
    classified to the reverse path.
    
    This patch updates the ACL flow check logic to account for the above.
    
    Change-Id: I2d8b9e6c2a1033d469668db29b9c18f525e89370
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

--- sfc/tests/functest/sfc_two_chains_SSH_and_HTTP.py
+++ sfc/tests/functest/sfc_two_chains_SSH_and_HTTP.py
@@ -185,7 +185,8 @@ def main():
     # Start measuring the time it takes to implement the classification rules
     t1 = threading.Thread(target=odl_utils.wait_for_classification_rules,
                           args=(ovs_logger, compute_nodes, odl_ip,
-                                odl_port, openstack_sfc.get_compute_client(),))
+                                odl_port, openstack_sfc.get_compute_client(),
+                                [neutron_port],))
 
     try:
         t1.start()
@@ -275,7 +276,8 @@ def main():
     # Start measuring the time it takes to implement the classification rules
     t2 = threading.Thread(target=odl_utils.wait_for_classification_rules,
                           args=(ovs_logger, compute_nodes, odl_ip,
-                                odl_port, openstack_sfc.get_compute_client(),))
+                                odl_port, openstack_sfc.get_compute_client(),
+                                [neutron_port],))
     try:
         t2.start()
     except Exception as e:
