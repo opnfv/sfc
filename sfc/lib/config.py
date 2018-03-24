a1b3feb74c4699e8781e982f91b4afff7e4f72b5
Manuel Buil <mbuil@suse.com>
2018-03-24 17:07:21 +0100

    Use different config files depending on version
    We can read the openstack version from the env variables and then use
    a different config. The reason we use this is because openstack API
    changed from Pike to Queens and Pike is the official version for
    OPNFV Fraser release
    
    Change-Id: I8a94e71224d2ba0aa75a6438f3d7b03929acbbef
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- sfc/lib/config.py
+++ sfc/lib/config.py
@@ -43,7 +43,18 @@ class CommonConfig(object):
         self.vnffgd_dir = os.path.join(self.sfc_test_dir, "vnffgd-templates")
         self.functest_results_dir = os.path.join(
             getattr(config.CONF, 'dir_results'), "odl-sfc")
-        self.config_file = os.path.join(self.sfc_test_dir, "config.yaml")
+
+        if os.getenv['OPENSTACK_OSA_VERSION'] is not None:
+            if os.environ['OPENSTACK_OSA_VERSION'] == 'stable/pike':
+                self.config_file = os.path.join(self.sfc_test_dir,
+                                                "config.yaml")
+            else:
+                self.config_file = os.path.join(self.sfc_test_dir,
+                                                "config-queens.yaml")
+        else:
+            self.config_file = os.path.join(self.sfc_test_dir,
+                                            "config.yaml")
+
         self.vim_file = os.path.join(self.sfc_test_dir, "register-vim.json")
 
         self.installer_type = env.get('INSTALLER_TYPE')
