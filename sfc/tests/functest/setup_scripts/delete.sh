1073b3038dad0bb3adc8cfdd441f80ca56f2c10a
Manuel Buil <mbuil@suse.com>
2019-03-18 15:31:37 +0100

    Remove set_scripts directory
    The contents of this directory are not used anymore
    
    Change-Id: I1dff86aba72288c618b4909ffa6197de6b241e49
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- sfc/tests/functest/setup_scripts/delete.sh
+++
@@ -1,8 +0,0 @@
-# Remember to source the env variables $creds before
-FILE=$(readlink -f $0)
-FILE_PATH=$(dirname $FILE)
-cd $FILE_PATH
-python ../../../lib/cleanup.py $1 $2
-openstack server delete client
-openstack server delete server
-for line in $(openstack floating ip list);do openstack floating ip delete $line;done
--- sfc/tests/functest/setup_scripts/delete_symmetric.sh
+++
@@ -1,9 +0,0 @@
-# Remember to source the env variables $creds before
-tacker sfc-classifier-delete red_http
-tacker sfc-classifier-delete red_http_reverse
-tacker sfc-delete red
-tacker vnf-delete testVNF1
-tacker vnfd-delete test-vnfd1
-nova delete client
-nova delete server
-for line in $(neutron floatingip-list | cut -d" " -f2);do neutron floatingip-delete $line;done
